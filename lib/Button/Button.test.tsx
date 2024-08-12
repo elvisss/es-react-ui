24856d24cf36d7283af4055ee809ae18d0879a8d
Elvis Saavedra <elvis.saavedra@nearsure.com>
2024-08-12 01:00:45 -0500

    add vitest

---
+++ lib/Button/Button.test.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react'
+import { Button } from './Button'
+
+describe(`Component: ${Button.name}`, () => {
+  it('should render', () => {
+    const { container } = render(<Button>My Button</Button>)
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <button
+          class="bg-red-800"
+        >
+          My Button
+        </button>
+      </div>
+    `)
+  })
+})
